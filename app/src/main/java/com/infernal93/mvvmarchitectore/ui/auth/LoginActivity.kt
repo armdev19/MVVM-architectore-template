5ef8ca4cdf1299c8c1bcb5096f1acd6d3b3cbf91
armdev19 <armdev19@gmail.com>
2019-12-23 19:58:35 +0400

    Create Login and Sign In design Fix gradle Bug (Room annotationProcessor)

--- app/src/main/java/com/infernal93/mvvmarchitectore/ui/auth/LoginActivity.kt
+++ app/src/main/java/com/infernal93/mvvmarchitectore/ui/auth/LoginActivity.kt
@@ -1,12 +1,13 @@
-package com.infernal93.mvvmarchitectore
+package com.infernal93.mvvmarchitectore.ui.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.infernal93.mvvmarchitectore.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
---
+++ app/src/main/java/com/infernal93/mvvmarchitectore/ui/auth/SignUpActivity.kt
@@ -0,0 +1,13 @@
+package com.infernal93.mvvmarchitectore.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.infernal93.mvvmarchitectore.R
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
